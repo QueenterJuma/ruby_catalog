15518d589dfd2c618eaa52a47d091d0336012622
QueenterJuma <jumahqueenter@gmail.com>
2023-05-29 17:11:02 +0300

    Add def listoption in main.rb

---
+++ main.rb
@@ -0,0 +1,14 @@
+require_relative 'app'
+
+def list_options
+  puts "Please select an option:"
+  puts "1. List all books"
+  puts "2. List all music albums"
+  puts "3. List all games"
+  puts "4. List all genres"
+  puts "5. List all authors"
+  puts "6. List all labels"
+  puts "7. Add a book"
+  puts "8. Add a music album"
+  puts "0 - Exit"
+end
