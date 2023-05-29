7daf413c4b5a06b2ddc7c88ff095036ded0ea946
Adholah96 <adholah96@gmail.com>
2023-05-29 17:30:28 +0300

    Implement options and main method

--- main.rb
+++ main.rb
@@ -10,5 +10,48 @@ def list_options
   puts "6. List all labels"
   puts "7. Add a book"
   puts "8. Add a music album"
+  puts "9. Add a game"
   puts "0 - Exit"
+end
+
+def option(app)
+  option = gets.chomp.to_i
+  case option
+  when 1
+    app.list_books
+  when 2
+    app.list_music_albums
+  when 3
+    app.list_games
+  when 4
+    app.list_genres
+  when 5
+    app.list_authors
+  when 6
+    app.list_labels
+  when 7
+    app.add_book
+  when 8
+    app.add_music_album
+  when 9
+    app.add_game
+  when 0
+    app.save_data
+    exit
+  else
+    puts 'Invalid option, please type correct number!'
+  end
+end
+
+def main
+  app = App.new
+  app.load_data
+
+  loop do
+    puts list_options
+    puts 'Welcome to our App..'
+    print 'Please select an option:'
+    option = gets.chomp.to_i
+    option(app)
+  end
 end
