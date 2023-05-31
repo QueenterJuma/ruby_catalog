1b208efdc1da2ed24c7a43384c740323f483504a
QueenterJuma <jumahqueenter@gmail.com>
2023-05-31 06:10:49 -0700

    Add update in app.rb

--- app.rb
+++ app.rb
@@ -1,28 +1,26 @@
 require_relative 'book'
-require_relative 'app'
 require_relative 'item'
 require_relative 'label'
 require_relative 'books_ui'
+require_relative 'game/game_mod'
+
 
 
 class App
-attr_accessor :books, :labels
+  attr_accessor :books, :labels, :game
 
-    def initialize
-@books = Books_UI.new
-    end
+  def initialize
+    @books = Books_UI.new
+    @game = GameUI.new
+  end
 
-def save_data
-@books.save_books
-@books.save_labels
-end
+  def save_data
+    @books.save_books
+    @books.save_labels
+  end
 
-
-def load_data
-@books.load_books
-@books.load_labels
-end
-
-
-
+  def load_data
+    @books.load_books
+    @books.load_labels
+  end
 end
