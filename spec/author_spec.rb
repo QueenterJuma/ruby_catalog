a5177555f3089155442a515ea0489d5b902b1421
QueenterJuma <jumahqueenter@gmail.com>
2023-05-31 11:21:27 -0700

    Add outher_spec.rb

---
+++ spec/author_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../Classes/item'
+require_relative '../Classes/Game/author'
+describe Author do
+  context 'With valid parameter, #add_item' do
+    it 'should add the item to the items array' do
+      author = Author.new('john', 'coughman')
+      item = Item.new('2022-03-14')
+      author.add_item(item)
+      items_array = author.items
+
+      expect(items_array.size).to eq(1)
+    end
+  end
