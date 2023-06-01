f7ee05a3208ba9f6b481a53d25db407dd0211f51
QueenterJuma <jumahqueenter@gmail.com>
2023-05-31 22:53:27 -0700

    Add update to author_spec

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,4 +1,4 @@
-require './game/author'
+require_relative '../game/author'
 
 describe Author do
   context '#add_item' do
@@ -9,14 +9,6 @@ describe Author do
     it 'returns correct Last name' do
       author = Author.new('Queen', 'Juma')
       expect(author.last_name).to eq('Juma')
-    end
-    it 'Test for add item method' do
-      author = Author.new('Queen', 'Juma')
-      item = double('Item')
-      allow(item).to receive(:publish_date) { '2021-08-10' }
-      allow(item).to receive(:author=)
-      author.add_item(item)
-      expect(author.items[0].publish_date).to eq('2021-08-10')
     end
   end
 end
