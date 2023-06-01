90d9eed0378b1dbae923312b28a977d1b6058f47
QueenterJuma <jumahqueenter@gmail.com>
2023-05-31 21:54:09 -0700

    Add update game_spec.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,20 @@
-require_relative '../Classes/Game/game'
+require './game/game'
 
 describe Game do
-  context '#can_be_archive when called' do
-    it 'should return false if @last_played_at > 2' do
-      game = Game.new('2010-03-10', 'yes', '2022-03-21')
-      result = game.can_be_archived?
+  before :each do
+    @game = Game.new('2021-08-10', 'Y', last_played_at: '2023-04-11')
+  end
 
-      expect(result).to eq(false)
-    end
+  it 'Test for Game instance' do
+    expect(@game).to be_instance_of Game
+  end
+
+  it 'Test for attribute' do
+    expect(@game).to have_attributes(publish_date: Date.parse('2021-08-10'), multiplayer: 'Y',
+                                     last_played_at: '2023-04-11')
+  end
+
+  it 'Test for can be archive method' do
+    expect(@game.send('can_be_archived?')).to be false
   end
 end
