b3abc288c3ccc20dfe1550fd38b02ec61581acc6
QueenterJuma <jumahqueenter@gmail.com>
2023-05-31 21:53:31 -0700

    Add update  author_spec.rb

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,13 +1,22 @@
-require_relative '../Classes/item'
-require_relative '../Classes/Game/author'
-describe Author do
-  context 'With valid parameter, #add_item' do
-    it 'should add the item to the items array' do
-      author = Author.new('john', 'coughman')
-      item = Item.new('2022-03-14')
-      author.add_item(item)
-      items_array = author.items
+require './game/author'
 
-      expect(items_array.size).to eq(1)
+describe Author do
+  context '#add_item' do
+    it 'returns correct First name' do
+      author = Author.new('Queen', 'Juma')
+      expect(author.first_name).to eq('Queen')
+    end
+    it 'returns correct Last name' do
+      author = Author.new('Queen', 'Juma')
+      expect(author.last_name).to eq('Juma')
+    end
+    it 'Test for add item method' do
+      author = Author.new('Queen', 'Juma')
+      item = double('Item')
+      allow(item).to receive(:publish_date) { '2021-08-10' }
+      allow(item).to receive(:author=)
+      author.add_item(item)
+      expect(author.items[0].publish_date).to eq('2021-08-10')
     end
   end
+end
